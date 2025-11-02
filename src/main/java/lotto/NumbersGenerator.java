9255fe2c42c7e9fb16673da06ac92360c5cf9c7a
cyeji <98408267+cyeji@users.noreply.github.com>
2025-11-02 16:09:23 +0900

    feat(lotto): 로또 발행 기능 추가
    NumbersGenerator, LottoMachine 추가 및 Lotto 검증/불변화 구현

---
+++ src/main/java/lotto/NumbersGenerator.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 무작위로 1~45 범위의 중복 없는 6개 숫자를 생성하여 오름차순으로 반환하는 유틸
+ */
+public class NumbersGenerator {
+
+    public static List<Integer> generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+        return numbers;
+    }
+}
+
